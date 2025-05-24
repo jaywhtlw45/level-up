5d226e4ddfd6c3db5e3e7057a767ee4fca64272d
Jason Whitlow <jason.whitlow45@gmail.com>
2025-05-24 00:40:14 -0700

    add design file

--- frontend/scripts/date-update.js
+++ frontend/scripts/date-update.js
@@ -1,7 +1,13 @@
-const date = new Date();
-sessionStorage.setItem("date", date.toISOString())
+const date = new Date()
+const formattedDateObj = { date: date.toISOString() }
+
+
+console.log("hello")
+
+sessionStorage.setItem("date", JSON.stringify(formattedDateObj))
+sessionStorage.getItem('date')
 
 
 // use this instead of window.onload
 // loads as soon as the dom content is loaded, does not wait for images
-document.addEventListener('DOMContentLoaded', updateDate);
+// document.addEventListener('DOMContentLoaded', updateDate)
